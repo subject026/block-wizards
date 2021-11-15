81d84b31e2f3b3d94635421b3a4dc6d9d85cb8a5
subject026 <subject026@protonmail.com>
2021-11-15 19:14:35 +0000

    woof

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React, { useState, createRef } from "react";
+import Wizard from "./components/Wizard";
+import { saveAsPng } from "./components/Canvas";
+
+const parts = ["background", ""];
+
+const initialState = {
+  color: "",
+  hue: Math.floor(Math.random() * 360),
+  sat: 90,
+  light: 10,
+};
+
+function App() {
+  const [state, setState] = useState({
+    ...initialState,
+    color: `hsl(${initialState.hue}, ${initialState.sat}%, ${initialState.light}%)`,
+  });
+
+  const svg = createRef(null);
+
+  const handleInputChange = ({ target: { value, name } }) => {
+    console.log(name, value);
+    const newState = { ...state };
+    newState[name] = value;
+    const { hue, sat, light } = newState;
+    const color = `hsl(${hue}, ${sat}%, ${light}%)`;
+    setState(() => ({ ...newState, color }));
+  };
+
+  const handleClick = async () => {
+    console.log(svg.current);
+    const result = await saveAsPng({ svg: svg.current });
+    console.log(result);
+  };
+
+  const { hue, sat, light, color } = state;
+
+  return (
+    <div className="min-h-screen bg-gray-300 p-4 sm:p-12 flex flex-col justify-center items-center">
+      <div>
+        <Wizard svg={svg} color={color} />
+        <div className="p-2 sm:p-12 md:px-32 bg-gray-200 uppercase">
+          <button className="" onClick={handleClick}>
+            save
+          </button>
+          <label className="block p-4 mb-4 sm:mb-8 bg-gray-100">
+            Hue
+            <input
+              name="hue"
+              id="hue"
+              className="ml-4 mb-4"
+              type="number"
+              step="1"
+              min="0"
+              max="360"
+              value={hue}
+              onChange={handleInputChange}
+            />
+            <input
+              name="hue"
+              onChange={handleInputChange}
+              type="range"
+              min="0"
+              max="360"
+              value={hue}
+            />
+          </label>
+          <label className="block p-2 mb-4 bg-gray-100">
+            Saturation
+            <input
+              className="m-4"
+              name="sat"
+              type="number"
+              step="1"
+              min="0"
+              max="100"
+              value={sat}
+              onChange={handleInputChange}
+            />
+            <input
+              name="sat"
+              onChange={handleInputChange}
+              type="range"
+              min="0"
+              max="100"
+              value={sat}
+            />
+          </label>
+          <label className="block p-4 bg-gray-100">
+            Lightness
+            <input
+              className="mb-4 mr-4"
+              name="light"
+              type="number"
+              step="1"
+              min="0"
+              max="100"
+              value={light}
+              onChange={handleInputChange}
+            />
+            <input
+              name="light"
+              onChange={handleInputChange}
+              type="range"
+              min="0"
+              max="100"
+              value={light}
+            />
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Wizard.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Wizard = ({ color, svg }) => {
+  console.log(color);
+  return (
+    <div>
+      <svg
+        style={{ height: "100%", shapeRendering: "crispEdges" }}
+        viewBox="0 0 6.615 6.615"
+        xmlns="http://www.w3.org/2000/svg"
+        ref={svg}
+      >
+        <g>
+          <path
+            style={{
+              fill: "#c8beb7",
+            }}
+            d="M0 0h6.615v6.615H0z"
+          />
+        </g>
+        <g>
+          <path
+            style={{ fill: "#d38d5f" }}
+            d="M2.117 2.117v.264h-.265v.794h-.265v.53h.265v2.91h1.323v-.53h1.058v-.264h.265v-.265h.264V2.381h-.264v-.264h-.265Z"
+          />
+        </g>
+        <g>
+          <path
+            style={{ fill: "#000" }}
+            d="M9 7v1h7V7Zm7 1v1h1V8Zm1 1v12h1V9Zm0 12h-1v1h1zm-1 1h-5v3h1v-2h4zM9 8H8v1h1ZM8 9H7v3h1Zm-1 3H6v2h1zm0 2v11h1V14Z"
+            transform="scale(.26458)"
+          />
+        </g>
+        <g>
+          <path style={{ fill: "#000" }} d="M3.175 5.027h.794v.265h-.794z" />
+        </g>
+        <g>
+          <path
+            style={{ fill: color }}
+            d="M3.969 2.91h.794v.794h-.794zM2.381 2.91h.794v.794h-.794z"
+          />
+        </g>
+        <path
+          style={{ fill: "#000" }}
+          d="M7 10v1h1v3h1v-3h3v3h1v-2h1v2h1v-3h3v3h1v-4H7zm11 4h-3v1h3v-1zm-6 0H9v1h3v-1zm-5-3H6v1h1v-1z"
+          transform="scale(.26458)"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Wizard;
